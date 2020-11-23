344337fd9fbb000f71f8b506f594d497b40a5fb9
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2020-11-23 09:43:48 +0100

    Update stencil evaluation

--- proseminar/04/stencil/evaluation.py
+++ proseminar/04/stencil/evaluation.py
@@ -4,19 +4,32 @@ import matplotlib.pyplot as plt
 
 # %%
 
-data = pd.read_csv(f"performance_full.csv", names=[
-                   'num_threads', 'N', 'time', 'valid'])
+for variant in ['full', 'optimized']:
+    data = pd.read_csv(f"performance_{variant}.csv", names=[
+        'num_threads', 'N', 'time', 'valid'])
 
-plt.figure(figsize=(12, 12))
-plt.title("Execution times without optimization")
+    fig, axs = plt.subplots(2, 1, figsize=(12, 12))
+    plt.suptitle(f"Performance plot for variant: {variant}")
+    axs[0].set_title("Speedup")
+    axs[0].set_xlabel("Number of steps")
+    axs[0].set_ylabel("Relative speedup")
 
-for num_threads in [1, 2, 4, 8]:
-    reduced_data = data[data['num_threads'] == num_threads]
-    ns = reduced_data['N']
-    times = reduced_data['time']
+    axs[1].set_title("Efficiency")
+    axs[1].set_xlabel("Number of steps")
+    axs[1].set_ylabel("Efficiency")
 
-    plt.plot(ns, times, label=f"{num_threads} threads")
+    baseline = data[data['num_threads'] == 1]['time'].reset_index(drop=True)
 
-plt.legend()
-# plt.show()
-plt.savefig('performance_full.png')
+    for num_threads in [2, 4, 8]:
+        reduced_data = data[data['num_threads'] == num_threads]
+        ns = reduced_data['N']
+        times = reduced_data['time'].reset_index(drop=True)
+        speedup = baseline/times
+        efficiency = speedup/num_threads
+
+        axs[0].plot(ns, speedup, label=f"{num_threads} threads")
+        axs[1].plot(ns, efficiency, label=f"{num_threads} threads")
+
+    plt.legend()
+    # plt.show()
+    plt.savefig(f'performance_{variant}.png')

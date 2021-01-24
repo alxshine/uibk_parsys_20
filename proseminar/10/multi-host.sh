0b88f30525da9588a0673168bc74bed6377306f6
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2021-01-24 09:12:24 +0100

    Fix errors in multi-host, launch not necessary

--- proseminar/10/multi-host.sh
+++ proseminar/10/multi-host.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-for p in {1,2,4,8};
+for p in {1,2,4}
 do
-  qsub -pe openmpi-${p}perhost $p -N "chapel-multinode-$p" -o "chapel-multinode-${p}.log" launch-multinode.script
+  let num_cpus=$p*8
+  for bin in {"pi","matrix"}
+  do
+    qsub -j y -q std.q -cwd -pe openmpi-8perhost $num_cpus -N $bin-m$p -o ${bin}-multinode-${p}.log << EOF
+`pwd`/${bin}_real -nl $p
+EOF
+  done
 done
